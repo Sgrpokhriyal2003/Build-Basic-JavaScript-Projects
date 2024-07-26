58259d03c02dcead307c71e0d31f448773c4d133
Sagar Pokhriyal <161199126+Sgrpokhriyal2003@users.noreply.github.com>
2024-07-26 19:37:41 +0530

    Built Stop-Watch Using JS
    Use Date built in function

---
+++ StopWatch/script.js
@@ -0,0 +1,48 @@
+const display = document.getElementById('display');
+// const startButton = document.getElementById('startBtn');
+// const stopButton = document.getElementById('stopBtn');
+// const restartButton = document.getElementById('restartBtn');
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+//start function
+function start(){
+    if(!isRunning){
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(update, 10);
+        isRunning = true;
+    }
+}
+//stop function 
+function stop(){
+    if(isRunning){
+        clearInterval(timer);
+        elapsedTime = Date.now - startTime;
+        isRunning = false;
+    }
+}
+//restart function
+function reset(){
+    clearInterval(timer);
+    startTime = 0;
+    elapsedTime = 0;
+    isRunning = false;
+    display.textContent = "00:00:00:00";
+}
+function update(){
+    const currentTime = Date.now();
+    elapsedTime = currentTime-startTime;
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60))
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let miliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    miliseconds = String(miliseconds).padStart(2, "0");
+
+    display.textContent = `${hours}: ${minutes}: ${seconds}: ${miliseconds} `
+
+}
